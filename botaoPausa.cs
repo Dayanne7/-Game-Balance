c046ac318d0fda1548245917505af70d1dc5f494
Day <43552096+Dayanne7@users.noreply.github.com>
2021-12-10 18:04:46 -0300

    Add files via upload

---
+++ botaoPausa.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botaoPausa : MonoBehaviour
+{
+    public bool flag; //true se for o botao de pause da tela de pause, false se for o pause do jogo
+    public Canvas pauseCanvas;
+
+    public void pauseGame()
+    {
+        if (flag)
+        {
+            pauseCanvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+        else
+        {
+            pauseCanvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
